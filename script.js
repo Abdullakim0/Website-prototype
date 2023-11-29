e8514a97bf02487bf95276e531cfca104732f3e2
Abdullakim0 <149297845+Abdullakim0@users.noreply.github.com>
2023-11-29 22:13:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿function register() {
+    // Dummy registration logic
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+
+    // Display registration result
+    var resultDiv = document.getElementById("registrationResult");
+    resultDiv.innerHTML = "Registration successful for " + username + " with email " + email;
+}
